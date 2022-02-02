69d84013cb714edb28e924167ce5ed0b92d7f322
smiiiiin <89795739+smiiiiin@users.noreply.github.com>
2022-02-03 01:54:34 +0900

    백준 7662: PQ(정렬되지 않은)

--- main.cpp
+++ main.cpp
@@ -1,116 +1,116 @@
+//정렬되지 않은 PQ
 #include<iostream>
-#include <string>
+#include<string>
+#include<vector>
 using namespace std;
 
-class Node{
+class PQ{
 private:
-    int data;
-    Node* next;
-public:
-    Node(int e){
-    this->data=e;
-    this-> next= nullptr;}
-    friend class Q;
-};
-
-class Q{
-private:
-    Node* fron;
-    Node* rear;
-    int sz;
+    int root;
+    vector<int> v;
     
 public:
-    Q(){
-    this-> fron= nullptr;
-    this-> rear= nullptr;
-    this->sz=0;
+    PQ(){
+    this-> root=0;
+    
     }
-    
     bool empty();
     int size();
-    void push(int);
-    int pop();
-    int front();
-    int back();
+    void enqueue(int e);
+    int findMin();
+    int findMax();
+    void delMin();
+    void delMax();
+
 };
 
-bool Q::empty(){
-return (sz==0); }
+bool PQ::empty(){
+    return v.size()==0;}
 
-int Q::size(){
-    return sz;}
-
-void Q::push(int elem){
-    //sz++; 하면 바로 empty일 수가 없다!!!
-    Node* newnode= new Node(elem);
-    if(empty()){rear=newnode;
-                fron=newnode;
+int PQ::size(){
+    int sz{0};
+    for(vector<int>::iterator i=v.begin();i<=v.end();i++){
         sz++;
     }
-    else{
-        rear->next=newnode;
-        rear=newnode;
-        sz++;
-    }
+    return sz;
+}
 
+void PQ::enqueue(int e){
+    v.push_back(e);
 }
-int Q::pop(){
-   
-    //fron을 기준으로해야함, empty나 size로 하면 안됨.
-    if(fron==nullptr) {return -1;}//rear뒤에 fron만 움직여서 empty가 아니게 되므로 sz기준으로 정리하자.
-    else {
-        Node* cur=fron;
-        fron= fron->next;
-        sz--;
-        return cur->data;}
+int PQ::findMin(){
+int min=9999999;
+
+for(vector<int>::iterator iter=v.begin();iter<=v.end();iter++){
+    if(*iter<=min){
+min=*iter;}
 }
-int Q::front(){
-    //
-    if(fron==nullptr) return -1;
-    else return fron->data;
+return min;}
+
+void PQ::delMin(){
+int min=9999999;
+
+for(vector<int>::iterator iter=v.begin();iter<=v.end();iter++){
+    if(*iter<=min){
+min=*iter;
+        iter++;//이걸 안하면 어떡해!
+    }
+    v.erase(iter);
+}}
+
+
+
+int PQ::findMax(){
+
+int max=0;
+
+for(vector<int>::iterator iter=v.begin();iter<=v.end();iter++){
+    if(*iter>=max){
+        max=*iter;}
 }
-int Q::back(){
-    if(empty()) return -1;
-    else return rear->data;
-}
+return max;}
+
+void PQ::delMax(){
+
+int max=0;
+for(vector<int>::iterator iter=v.begin();iter<=v.end();iter++){
+    if(*iter>=max){
+        max=*iter;
+        iter++;
+    }
+    v.erase(iter);
+}}
 
 
 
 int main(){
-    Q que;
-    int num, da, i{0};
+    PQ pqda;
     string oper;
-    cin>>num;
+    int play, num, da, i{0};
+    cin>>play;
+    for(int j=0;j<play;j++){ //게임을 몇번 할건지
+        cin>>num;
+        while(i!=num){
+            cin>>oper;
+            
+            if(oper=="D -1"){
+                pqda.delMin();
+                i++;
+                }
+            
+            else if(oper=="I"){
+                cin>>da;
+                pqda.enqueue(da);
+                i++;
+                }
+            
+            else if(oper=="D 1"){
+                pqda.delMax();
+                i++;
+            }}
+        cout<<pqda.findMax()<<" ";
+        cout<<pqda.findMin();
+    }
     
-    while(i!=num){
-        cin>>oper;
-        
-        if(oper=="size"){
-            cout<<que.size()<<endl;
-            i++;
-        }
-        else if(oper=="empty"){
-    cout<<que.empty()<<endl;
-            i++;
-        }
-        else if(oper=="push"){
-            cin>>da;
-            que.push(da);
-            i++;
-        }
-        else if(oper=="front"){
-            cout<<que.front()<<endl;
-            i++;
-        }
-        else if(oper=="pop"){
-            cout<<que.pop()<<endl;
-            i++;
-        }
-        else if(oper=="back"){
-            cout<<que.back()<<endl;
-            i++;
-        }
-    
-   }
 return 0;
 }
