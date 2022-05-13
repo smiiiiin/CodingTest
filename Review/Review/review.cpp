2e74502c649b924148dabac40b75677de461c3c1
sumin <sumin971212@gmail.com>
2022-05-13 23:52:44 +0900

    dp혼공

---
+++ Review/Review/review.cpp
@@ -0,0 +1,21 @@
+//백준 11727 2xn 타일링 2
+
+#include<iostream>
+#include<algorithm> //min
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
+
+/*
+ 눕힌거 2가지로 늘어나거나 or 세운거 하나로 늘어나거나
+ 
+ n=1 1가지
+ n=2 dp[1]|, 눕힌거 2가지
+ n=3 dp[1]눕힌거2가지,
+ n=4 dp[3]에 세로하나, dp[2]에 눕힌거 2가지로.
+ 
+ 
+ */
