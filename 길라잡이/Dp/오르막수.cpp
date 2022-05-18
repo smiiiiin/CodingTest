f220b08266c62787c0346ea8047f4bc33a58f234
Smiiiiin <89795739+smiiiiin@users.noreply.github.com>
2022-05-18 19:30:11 +0900

    정리완

--- 길라잡이/Dp/오르막수.cpp
+++ 길라잡이/Dp/오르막수.cpp
@@ -1,30 +1,18 @@
-//백준 11057
-//long long 하고 범위를 크게 써야지 안틀린다. 
+//백준 11057 오르막 수 
+//long long 하고 범위를 크게 써야지 안틀린다. //괄호도 주의해라 
 #include <iostream>
 #define MOD 10007
 using namespace std;
-int n;
-long long dp[10][1005];
-long long ret;
+int n; long long dp[1005][10]={0,}; long long ret{0};
 
-int main(void)
-{
-    cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(false);
-    cin>>n;
-    for(int i=1;i<=n;i++) dp[0][i]=1;
-    for(int i=0;i<10;i++) dp[i][1]=1;
+int main(void){
+    cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(false); cin>>n;
+    for(int i=0;i<10;i++) dp[1][i]=1;  for(int i=1;i<=n;i++) dp[i][0]=1; 
     
-    
-    for(int x=2;x<=n;x++) {
-        for(int y=1;y<=9;y++){
-            dp[y][x]= (dp[y-1][x]+dp[y][x-1])% MOD;
-        }
-    }
-    ret=0;
-    for(int i=0;i<=9;i++){
-        ret+= dp[i][n]% MOD; //MOD 금지
-    }
-    
-    cout<<ret % MOD;
-    return 0;
-}
+    for(int y=2;y<=n;y++){
+        for(int x=1;x<10;x++){
+            dp[y][x]= (dp[y-1][x]+dp[y][x-1]) %MOD;  }}
+    for(int i=0;i<10;i++) ret= (ret+ dp[n][i]) %MOD;
+    cout<<ret%MOD<<"\n";
+    return 0;}
+
