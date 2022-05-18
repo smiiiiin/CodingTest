49c42b404a9ed9e41e76b9232a383c21cae80f78
Smiiiiin <89795739+smiiiiin@users.noreply.github.com>
2022-05-18 15:13:37 +0900

    정리완

--- 길라잡이/Dp/스티커.cpp
+++ 길라잡이/Dp/스티커.cpp
@@ -1,7 +1,6 @@
 //9465: sticker
 //index크기 존나게 유의하삼: 이거때문에 1시간 태움
 
-
 #include <iostream>
 #include <algorithm>
 using namespace std;
@@ -9,23 +8,24 @@ int t; int n;
 long long dp[2][100003];
 
 int main(void)
-{   scanf("%d", &t);
+{   cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(false);
+    cin>>t;
     
     while(t--){
-        scanf("%d", &n);
+        cin>>n;
     for(int y=0;y<2;y++){
-        for(int x=1;x<=n;x++){
-            scanf("%d", &dp[y][x]);
+        for(int x=0;x<n;x++){
+            cin>>dp[y][x];
         }}
     
-    dp[0][2]+=dp[1][1]; dp[1][2]+=dp[0][1];
+    dp[0][1]+=dp[1][0]; dp[1][1]+=dp[0][0];
     
-    for(int x=3;x<=n;x++){
+    for(int x=2;x<n;x++){
         for(int y=0;y<2;y++){
             dp[y][x]+= max(dp[1-y][x-1], dp[1-y][x-2]);
         }
     }
-        printf("%d\n", max(dp[0][n], dp[1][n]));
+        cout<< max(dp[0][n-1], dp[1][n-1])<<"\n";
         
     }
     return 0;
