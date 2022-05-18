118ad112f3c7269f51c0a59a87f0317f719d7c34
Smiiiiin <89795739+smiiiiin@users.noreply.github.com>
2022-05-18 17:50:59 +0900

    ㅈㄴ어렵다

--- 길라잡이/Dp/암호코드.cpp
+++ 길라잡이/Dp/암호코드.cpp
@@ -1,21 +1,20 @@
-//백준 2011
-#include <iostream>
-#include <vector>
-#include <string>
+// 백준 2011 암호코드 
+// 완전 유의할 것: int dp[5001] = {1,1}; // dp의 0,1번째 값만  1
+#include<iostream>
+#include<string>
+#define mod 1000000
 using namespace std;
 
-#define MOD 1000000
-int dp[5050] = {1};
 int main(){
-    string s;
-    cin>>s;
-    s = '#'+s;
-    if(s[1] == '0') return cout<<0, 0;
-    for(int i = 1 ; i < (int)s.size() ; i++){
-        if(s[i]-'0'>0)dp[i] = (dp[i-1]+dp[i])%MOD;
-        if(i>1 and s[i-1]-'0'>0 and stoi(string({s[i-1], s[i]})) <= 26)
-            dp[i] = (dp[i-2]+dp[i])%MOD;
-    }
-    cout<<dp[(int)s.size()-1];
-}
+    string s; cin>>s; int size = s.size();
+    if(s[0]=='0'){ cout<<0; return 0;}
+    
+    int dp[5001] = {1,1}; // dp의 0,1번째 값만  1
+    for(int i=2; i<=size; i++){
+        //나의 전값이 0보다만 크다면 그대로 옮김
+        if(s[i-1]>'0') dp[i]=dp[i-1]%mod; 
+        //두자리씩 10-26일때 
+        int n =(s[i-2]-'0')*10 + s[i-1]-'0'; 
+        if(10<=n && 26>=n) dp[i] = (dp[i]+dp[i-2])%mod;}
+    cout<<dp[size];}
  
