ddaba1b4371916a17799c181b0cc24f2b46622b9
Smiiiiin <89795739+smiiiiin@users.noreply.github.com>
2022-05-18 18:52:19 +0900

    정리완(!)

--- 길라잡이/Dp/연속합.cpp
+++ 길라잡이/Dp/연속합.cpp
@@ -2,15 +2,12 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
+//음수가 들어갈때는 0을 활용해보자
 
-int main()
-{
-	int n;
-	cin>>n;
-	int arr[n],dp[n]={0,};
-	for(int&i:arr)cin>>i;
-	dp[0]=arr[0];
-	for(int i=1;i<n;i++)
-	dp[i]=max(0,dp[i-1])+arr[i];
-	cout<<*max_element(dp,dp+n);
-}
+int main(){
+	int n; cin>>n;
+    int arr[n], dp[n]={0,};
+    for(auto &i: arr) cin>>i; dp[0]=arr[0]; //auto만 쓰면 안된다, auto& 를 써야지 비상수참조자란다. 
+    //앞에서부터 차근차근왔는데 최선이 음수야 그럼 난 걔를 버려도 돼
+    for(int i=1;i<n;i++) dp[i]= max(0,dp[i-1])+arr[i];
+    cout<<*max_element(dp,dp+n)<<"\n"; return 0;}
