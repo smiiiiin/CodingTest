89d6830d7d21462cd70eb09373ab59df0c24acf2
Smiiiiin <89795739+smiiiiin@users.noreply.github.com>
2022-05-19 13:01:56 +0900

    정리완

--- 길라잡이/Dp/카드 구매하기.cpp
+++ 길라잡이/Dp/카드 구매하기.cpp
@@ -1,28 +1,14 @@
-// 백준알고리즘 11052번 :: 카드 구매하기
+// 백준 11052 카드 구매하기
 #include<iostream>
+#include<algorithm>
 using namespace std;
-#define max(a, b) (a > b) ? a : b
+int n;  int arr[1002]; int dp[1002];
  
-int N;
-int arr[1002];
-int dp[1002];
- 
-int main(void)
-{
-    cin >> N;
- 
-    for (int i = 1; i <= N; i++)
-        cin >> arr[i];
-    
-    dp[1] = arr[1];
- 
-    for (int i = 2; i <= N; i++)
-    {
-        for (int j = 0; j < i; j++) {
-            dp[i] = max(dp[i], dp[j] + arr[i - j]);
-        }
-    }
- 
-    cout << dp[N];
-    return 0;
-}
+int main(void){
+    //초기화 안한 DP[0]=0;이니까. 
+    cin>>n; for(int i=1;i<=n;i++) cin>>arr[i]; dp[1]=arr[1]; 
+    for(int i=2;i<=n;i++){
+        for(int j=0;j<i;j++){ //j=0을 해야 arr[]자기자신만 사는경우도 포함 
+            dp[i]=max(dp[i], dp[j]+arr[i-j]);}}
+    cout<<dp[n]<<"\n";
+    return 0;}
