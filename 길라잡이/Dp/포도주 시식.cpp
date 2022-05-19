d52a43555c321ebc05f53c580e817e5b7b0a3c9f
Smiiiiin <89795739+smiiiiin@users.noreply.github.com>
2022-05-19 13:54:56 +0900

    정리완

--- 길라잡이/Dp/포도주 시식.cpp
+++ 길라잡이/Dp/포도주 시식.cpp
@@ -1,24 +1,16 @@
-//백준 2156
-//2xN 타일링 문제와 비슷한 원리: 반복되는 단위의 최소를 찾는다. 
+// 백준 2156 포도주시식 
+// max({,,}); 이러면 3개중 max를 고를 수 있음 
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int n;
-long long a[10001];
-long long dp[10001];
+int n; long long arr[10001]; long long dp[10001];
 
-int main(void)
-{   cin>>n;
-    for(int i=1;i<=n;i++){
-        cin>>a[i];
-    }
-    dp[1]= a[1];
-    dp[2]= a[1]+a[2];
-    for(int j=3;j<=n;j++){
-        dp[j]= max({dp[j-1], a[j]+a[j-1]+dp[j-3], a[j]+dp[j-2]});
-    }
-    cout<<dp[n];
-    return 0;
-}
-
-
+int main(void){   
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>arr[i];
+    dp[1]=arr[1]; dp[2]= arr[1]+arr[2]; 
+    // 끝에서 두개 택했으면 그 전에것은 체크되면 안되니까 dp[i-3] 이런 식 
+    for(int i=3;i<=n;i++) dp[i]= max({dp[i-1], dp[i-2]+arr[i],dp[i-3]+arr[i]+arr[i-1]});
+    
+    cout<<dp[n]<<"\n";
+    return 0;}
