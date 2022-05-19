a1b2e7a6f11ceab892ea89949cb95cd3ad1af515
Smiiiiin <89795739+smiiiiin@users.noreply.github.com>
2022-05-19 12:19:50 +0900

    정리완

--- 길라잡이/Dp/제곱수의 합.cpp
+++ 길라잡이/Dp/제곱수의 합.cpp
@@ -1,22 +1,16 @@
-//백준 1699
-//백준 1699
+// 백준 1699
+// N=12인 경우를 생각해보자:)
 #include <iostream>
 #include <cmath>
 using namespace std;
 
-int dp[100001];
-const int INF = 987654321;
+int dp[100001]; const int INF = 987654321;
 
-int main()
-{ 	int N;
-	cin >> N;
-	for (int i = 0; i <= N; ++i){
-		dp[i] = i;}
+int main(){ 	
+    int N; cin >> N;
+	for (int i = 0; i <= N; ++i) dp[i] = i;
 	
 	for (int i = 2; i <= N; ++i){
 		for (int j = 1; j * j <= i; ++j){
-			dp[i] = min(dp[i], dp[i - j * j] + 1);}}
-			
-	cout << dp[N];
-}
-  
+			dp[i] = min(dp[i],dp[i-(j*j)]+1);}}
+	cout << dp[N];}
