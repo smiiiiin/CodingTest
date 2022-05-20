3f0c79b2c4791c94459bedce987b8c45b2af5dc6
Smiiiiin <89795739+smiiiiin@users.noreply.github.com>
2022-05-20 14:00:24 +0900

    정리완

--- 길라잡이/Brute Force/N과 M (2).cpp
+++ 길라잡이/Brute Force/N과 M (2).cpp
@@ -6,21 +6,21 @@ int n,m;
 int arr[8]={0,};
 bool visited[8]={0,};
 
-void dfs(int node, int cnt) { //브루트포스 
+void dfs(int depth, int node) { //브루트포스
     
-	if (cnt-1 == m) {
-		for(int i=1;i<=m;i++){
-		    cout<<arr[i]<<" ";}
-		    cout << '\n';
-		return; //그 전으로 돌아갓 (손으로 그린 그래프에서) 
-		}
-	
-	for(int i=node;i<=n;i++){
-	    if(!visited[i]) {
-	        visited[i]=1;
-	        arr[cnt]=i;
-	        dfs(i+1,cnt+1);
-	        visited[i]=0; }}
+    if (depth-1 == m) {
+        for(int i=1;i<=m;i++){
+            cout<<arr[i]<<" ";}
+            cout << '\n';
+        return; //그 전으로 돌아갓 (손으로 그린 그래프에서)
+        }
+    
+    for(int i=node;i<=n;i++){
+        if(!visited[i]) {
+            visited[i]=1;
+            arr[depth]=i;
+            dfs(depth+1, node+1);  // dfs(depth+1, node+1);은 안된다
+            visited[i]=0; }}
 }
  
 
