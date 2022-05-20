14743d5f08d833b4add68314a1c062307dfe9343
Smiiiiin <89795739+smiiiiin@users.noreply.github.com>
2022-05-20 12:03:40 +0900

    정리완

--- 길라잡이/Brute Force/N과 M (1).cpp
+++ 길라잡이/Brute Force/N과 M (1).cpp
@@ -9,18 +9,18 @@ using namespace std;
 
 int n,m; int arr[9]; bool visited[9];
 
-void dfs(int index) { 
+void dfs(int depth) {
     
-	if (index == m) {
-		for(int i=0;i<m;i++) cout<<arr[i]<<" ";
-		cout<<"\n"; return;}
-		
-	for(int i=1;i<=n;i++){ //i는 node에 대해서 , dfs()와 dfs() 사이의 것들을 못함. i=1일때 i=2,3,4,5 
-    	if (!visited[i]) {
-    		visited[i] = 1; arr[index]=i; 
-    		dfs(index+1); 
-    		visited[i]=0;
-    	    //cout<<"out: "<<i<<" "; //test해볼 수 있는 코드 
-    	}}}
+    if (depth-1 == m) {
+        for(int i=1;i<=m;i++) cout<<arr[i]<<" ";
+        cout<<"\n"; return;}
+        
+    for(int i=1;i<=n;i++){ //i는 node에 대해서 , dfs()와 dfs() 사이의 것들을 못함. i=1일때 i=2,3,4,5
+        if (!visited[i]) {
+            visited[i] = 1; arr[depth]=i;
+            dfs(depth+1);
+            visited[i]=0;
+            //cout<<"out: "<<i<<" "; //test해볼 수 있는 코드
+        }}}
 
-int main() {cin >> n>>m; dfs(0); }
+int main() {cin >> n>>m; dfs(1); }
