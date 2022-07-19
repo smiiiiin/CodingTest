50378e3bac6aaef0652f5176beee3df77ad78d3d
Smiiiiin <89795739+smiiiiin@users.noreply.github.com>
2022-07-19 18:07:22 +0900

    💛%

--- 길라잡이/Bfs:Dfs/텀 프로젝트.cpp
+++ 길라잡이/Bfs:Dfs/텀 프로젝트.cpp
@@ -1,209 +1,91 @@
-
-//나 하는중
-//백준 9466 텀 프로젝트
-#include<iostream>
-#include<cstring>
-#include<queue>
-#define MAX 100001
-using namespace std;
-
-int t,n,cnt; int arr[MAX]; int visited[MAX]; //처음시작노드
-
-void bfs(int start){
-    queue<int> q;
-    q.push(start);
-    while(!q.empty()){
-        int cur=q.front(); q.pop();
-        int next=arr[cur];
-        if(visited[next]!=-1 ) { visited[next]=cur;
-            q.push(next);}
-    }
-    
-    
-}
-
-int main(){
-    cin>>t;
-    while(t--){
-        memset(arr,-1,sizeof(arr)); memset(visited,-1,sizeof(visited));
-        cin>>n;
-        for(int i=0;i<n;i++) cin>>arr[i];
-        for(int i=0;i<n;i++){
-            if(visited[i]!=-1) bfs(i);
-        }
-        }
-    return 0;}
-
-
-
-
-
-
-
-
-
-
-
-
-
-//내가 dfs쓰는 거 포기하고 이걸로 하려고 하는데도 이해가 안간다. 
+//백준 9466 텀 프로젝트 : dfs 이용 이게더 이해하기 쉽다 나는 
 #include <iostream>
 #include <queue>
 #include <cstring>
 #define MAX 100001
 using namespace std;
+// 입력 arr[]은 내가 원하는 얘가 있으니까 자식노드라고 생각
+int t, n; int arr[MAX]; bool visited[MAX]; bool done[MAX]; int cnt;
 
-int t, n;
-int graph[MAX]; 
-bool visited[MAX];
-bool done[MAX];
-int cnt;
-void hasCycle( int node) {
+void dfs( int node) {
 
-	visited[node] = true;
-	int next = graph[node];
-
-	
-	if (!visited[next]) {
-		hasCycle( next);
-	}
-	else if (!done[next]) {//방문은 했지만 아직 사이클이 아니라면 next까지 포함해서 사이클 완성
-		//자기 자신을 포함한 팀의 멤버를 카운트
-		for (int i = next; i != node; i = graph[i]) {//여기가 포인트인데 모르겠다. 
-			cnt++;
-		}
-		cnt++;
-	}
-	done[node] = true;
-}
+    visited[node] = 1;
+    int next = arr[node];
+    
+    if (!visited[next]) dfs( next);
+    // visited[next]=1은 next인데 방문을 했다는 의미: back_edge거나 자기자신 가르키는 경우
+    else if (!done[next]) {//방문은 했지만 사이클done이 아니라면 next까지 포함해서 사이클 완성
+        //자기 자신을 포함한 팀의 멤버를 카운트
+        for (int i = next; i != node; i = arr[i]) { //자식노드로 연결해서 cycle완성
+            cnt++;
+        }
+        cnt++;}  //그리고 나자신까지 +1;
+    
+    done[node] = 1;} //여기만 이해가 안가
 
 
 int main() {
-
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	cin >> t;
-	while (t--) {
-		cin >> n;
-		for (int i = 1; i <= n; i++) {
-			cin >> graph[i];
-		}
-		for (int i = 1; i <= n; i++) {
-			if (!visited[i]) {
-				hasCycle(i); 
-			}
-		}
-		cout << n-cnt << '\n';
-		cnt = 0;
-		memset(visited, false, sizeof(visited));
-		memset(done, false, sizeof(done));
-	}
-
-
-	return 0;
-}
-
-
-
-//queue
-
-#include <bits/stdc++.h>
-using namespace std;
-
-int n, t;
-bool vis[100003];
-int student[100003];
-int startNumber[100003];
-int countNumber[100003];
-
-int main(){
-    cout.sync_with_stdio(0);
-    cin.tie(0);
-
-    cin >> t;
-    while(t--){
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin >> t; while (t--) {
+        // 초기화
+        cnt = 0; memset(visited, false, sizeof(visited)); memset(done, false, sizeof(done));
+        
         cin >> n;
-        int ans = n;
-        fill(startNumber, startNumber + n + 1, -1);
-        fill(countNumber, countNumber + n + 1, 0);
-        fill(vis, vis + n + 1, false);
-        for(int i = 1; i <= n; i++) {
-            cin >> student[i];
-        }
-        for(int i = 1; i <= n; i++) {
-            if(vis[i])continue;
-            queue<int> Q;
-            Q.push(i);
-            startNumber[i] = i;
-            vis[i] = true;
-            countNumber[i] = 1;
-            while(!Q.empty()) {
-                int cur = Q.front(); Q.pop();
-                vis[cur] = true;
-                int nextStudent = student[cur];
-                if(startNumber[nextStudent] == startNumber[cur]) {
-                    if(nextStudent == cur) {
-                        ans -= 1;
-                    } else {
-                        ans = ans - (countNumber[cur] - countNumber[nextStudent] + 1);
-                    }
-                    break;
-                }
-                if(vis[nextStudent]) continue;
-                startNumber[nextStudent] = startNumber[cur];
-                countNumber[nextStudent] = countNumber[cur] + 1;
-                Q.push(nextStudent);
-            }
-        }
-        cout << ans << '\n';
-    }
+        for (int i = 1; i <= n; i++) cin >> arr[i];
+        
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) dfs(i);}
+        
+        cout << n-cnt << '\n';}
+    return 0;}
 
 
-    return 0;
-}
 
-//
+//백준 9466 텀 프로젝트 : 다른정답: 움직이는 cur
 #include <iostream>
 #include <algorithm>
-#include <queue>
 using namespace std;
 
-int T, n;
-int choice[100002];
-int team[100002]; // 0은 안 간 곳, 숫자는 첨 시작한 사이클 번호
+int t, n; int arr[100002]; int visited[100002]; // 0은 안 간 곳, 숫자는 첨 시작한 사이클 번호
+
+void moving_cur(){
+    for (int starter = 1; starter <= n; starter++) {
+        if (visited[starter] == 0) {// 들른적 없으면
+            int cur = starter;
+            
+            
+            while (1) {
+                visited[cur] = starter; //방문한 걸 자기노드로 표시한다.
+                cur = arr[cur]; //cur의 이동
+                
+                
+                if (visited[cur] == starter) { //방문이 되어있으면 back_edge or자기자신
+                    while (visited[cur] != -1) {
+                        visited[cur] = -1; //사이클 안에 친구들을 다 -1로 만든다
+                        cur = arr[cur]; //cur은 사이클을 돌며 이동한다
+                    } break; //1번 브레이크
+                }
+                // visited[cur]이 0으로 아직 방문 안했다면 위로 가서 방문한다
+                
+                else if (visited[cur] != 0) break; // 2번 브레이크
+            }
+            //1,2번 브레이크 하면 starter부터 다시 간다
+        }}}
+
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> T;
-	while (T--) {
-		cin >> n;
-		int ans = 0;
-		for (int i = 1; i <= n; i++)
-			cin >> choice[i];
-		for (int i = 1; i <= n; i++) {
-			if (team[i] == 0) {
-				int cur = i;
-				while (1) {
-					team[cur] = i;
-					cur = choice[cur];
-					if (team[cur] == i) {
-						while (team[cur] != -1) {
-							team[cur] = -1;
-							cur = choice[cur];
-						}
-						break;
-					}
-					else if (team[cur] != 0) break;
-				}
-			}
-		}
-		for (int i = 1; i <= n; i++) {
-			if (team[i] != -1) ans++;
-			team[i] = 0;
-		}
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> t; while (t--) {
+        cin >> n; int ans = 0;
+        for (int i = 1; i <= n; i++) cin >> arr[i];
+        
+        moving_cur();
+        
+        for (int i = 1; i <= n; i++) { //다돌려가며
+            if (visited[i] != -1) ans++; //-1이 아니면 외톨이들 수 (=답)
+            visited[i] = 0; //초기화
+        }
 
-		cout << ans << "\n";
-	}
+        cout << ans << "\n";
+    }
 }
